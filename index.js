cf0def420ba300d6a210e3676a1c67cb09b891cb
Suchith <suchith@zeutec.com>
2026-04-01 18:07:46 +0530

    Added middleware example

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(Logger);   // middleware first
+
+function Logger(req,res,next){
+  console.log(`${req.method} ${req.url}`);
+  next(); 
+}
 
 app.get("/", (req, res) => {
   res.send("Hello Welcome to Express.js! This is the home page.");
@@ -27,6 +33,60 @@ app.post("/NewUser", (req, res) => {
     user: {name,role,age},
   });
 });
+
+/* Fake database */
+const users = [];
+
+/* Signup Route */
+app.post("/signup", (req, res) => {
+  const { email, password, role } = req.body;
+
+  users.push({ email, password, role });
+
+  res.json({
+    message: "User registered successfully",
+    user: { email, role }
+  });
+
+});
+
+/* Role Middleware */
+function checkRole(req,res,next){
+  const {email,password}=req.body;
+
+  const user=users.find(
+    u=>u.email===email && u.password===password
+  )
+  if(!user){
+    return res.status(401).json({
+      message:"Invalid credentials"
+    })
+  }
+  req.user=user;
+  next();
+}
+
+/* Signin Route */
+app.post("/signin", checkRole, (req, res) => {
+
+  if (req.user.role === "admin") {
+    return res.json({
+      message: "Welcome Admin! You have full access."
+      
+    });
+  }
+
+  if (req.user.role === "manager") {
+    return res.json({
+      message: "Welcome Manager! You have limited access."
+    });
+  }
+
+  res.json({
+    message: "Unknown role"
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
